82e331e1bd7c89fbeeb14afb1778fa4eef530be4
pi <pi@brokenrul.es>
2016-02-16 11:25:58 +0100

    File uploading added

---
+++ Http/UploadFile.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Unitilities {
+	public class FileUploader {
+
+		public IEnumerator UploadFileCo(string localFileName, string uploadURL, string mimeType = "text/plain")
+		{
+			WWW localFile = new WWW("file:///" + localFileName);
+			yield return localFile;
+			if (localFile.error == null)
+				Debug.Log("Loaded file successfully");
+			else
+			{
+				Debug.Log("Open file error: "+localFile.error);
+				yield break; // stop the coroutine here
+			}
+			WWWForm postForm = new WWWForm();
+			// version 1
+			//postForm.AddBinaryData("theFile",localFile.bytes);
+			// version 2
+			postForm.AddBinaryData("theFile",localFile.bytes, localFileName, mimeType);
+			WWW upload = new WWW(uploadURL,postForm);        
+			yield return upload;
+			if (upload.error == null)
+				Debug.Log("upload done :" + upload.text);
+			else
+				Debug.Log("Error during upload: " + upload.error);
+		}
+
+
+	}
+}
+
+public class UploadFile : MonoBehaviour {
+	readonly Unitilities.FileUploader _uploader = new Unitilities.FileUploader();
+
+	public void Upload(string localFileName, string uploadURL, string mimeType) {
+		StartCoroutine(_uploader.UploadFileCo(localFileName, uploadURL, mimeType));
+	}
+	public void UploadPNG(string localFileName, string uploadURL) {
+		StartCoroutine(_uploader.UploadFileCo(localFileName, uploadURL, "image/png"));
+	}
+	public void UploadText(string localFileName, string uploadURL) {
+		StartCoroutine(_uploader.UploadFileCo(localFileName, uploadURL));
+	}
+}
+
+
---
+++ Screenshot/Screenshooter.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.IO;
+
+namespace Unitilities {
+	namespace Screenshot {
+		public static class Screenshooter {
+			public static Texture2D TakeScreenshot() {
+				return TakeScreenshot(Screen.width, Screen.height, Camera.main);
+			}
+			public static Texture2D SaveScreenshot(int number) {
+				return TakeScreenshot(Screen.width, Screen.height, Camera.main, "Screenshot"+number+".png");
+			}
+
+			public static Texture2D TakeScreenshot(int width, int height, 
+				Camera screenshotCamera) {
+				if(width<=0 || height<=0) return null;
+				if(screenshotCamera == null) screenshotCamera = Camera.main;
+
+				Texture2D screenshot = new Texture2D(width, height, TextureFormat.RGB24, false);
+				RenderTexture renderTex = new RenderTexture(width, height, 24);
+				screenshotCamera.targetTexture = renderTex;
+				screenshotCamera.Render();
+				RenderTexture.active = renderTex;
+				screenshot.ReadPixels(new Rect(0, 0, width, height), 0, 0);
+				screenshot.Apply(false);
+				screenshotCamera.targetTexture = null;
+				RenderTexture.active = null;
+				renderTex.Release();
+				Object.Destroy(renderTex);
+				return screenshot;
+			}	
+
+			public static Texture2D TakeScreenshot(int width, int height, 
+				Camera screenshotCamera, string saveToFileName) {
+				Texture2D screenshot = TakeScreenshot(width, height, screenshotCamera);
+				if(screenshot != null && saveToFileName!=null) {
+					if(Application.platform==RuntimePlatform.OSXPlayer || 
+						Application.platform==RuntimePlatform.WindowsPlayer && 
+						Application.platform!=RuntimePlatform.LinuxPlayer 
+						|| Application.isEditor) {
+						byte[] bytes;
+						if(saveToFileName.ToLower().EndsWith(".jpg"))
+							bytes = screenshot.EncodeToJPG();
+						else 
+							bytes = screenshot.EncodeToPNG();
+
+						FileStream fs = new FileStream(saveToFileName, FileMode.OpenOrCreate);
+						BinaryWriter w = new BinaryWriter(fs);
+						w.Write(bytes);
+						w.Close();
+						fs.Close();
+					}
+				}
+				return screenshot;
+			}
+		}
+	}
+}
+
