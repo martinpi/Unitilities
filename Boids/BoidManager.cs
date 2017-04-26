af0f85120283f9fe9c7701f224c12a51714b5de0
pi <pi@brokenrul.es>
2017-04-26 19:40:21 +0200

    - boids don't spawn inside camera area

---
+++ Boids/BoidManager.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Unitilities.Boids;
+using Vectrosity;
+
+public class BoidManager : MonoBehaviour {
+
+	private List<Transform> _hostiles;
+
+	private Swarm _swarm;
+	[SerializeField] int numberOfDots = 20;
+
+	[SerializeField] float speed = 0.5f;
+	[SerializeField] float sight = 200f;
+	[SerializeField] float flockDistance = 10f;
+	[SerializeField] float collisionSize = 1f;
+	[SerializeField] float huntDistance = 500;
+	[SerializeField] float agility = 0.3f;
+	[SerializeField] Vector2 area;
+	[SerializeField] int type;
+
+	[SerializeField] GameObject Prefab;
+
+	[SerializeField] Transform target;
+	private GameManager _manager;
+
+	void Start () {
+		_manager = FindObjectOfType<GameManager>();
+		StartCoroutine(InitSwarms());
+	}
+
+	IEnumerator InitSwarms() {
+		Vector2 pos = transform.position.Vector2XZ();
+		Rect areaRect = new Rect(pos-area/2f, area);
+		_swarm = new Swarm(0.25f, areaRect, numberOfDots, type);
+
+		ObjectPool.instance.Init();
+
+		_hostiles = new List<Transform>();
+		for (var i = 0; i < numberOfDots; i++) 
+			_hostiles.Add(ObjectPool.instance.GetObjectForType(Prefab.name, false).transform);
+
+		UpdateSwarmParameters();
+		for (var i = 0; i < numberOfDots; i++) 
+			_swarm.Boids[i].Reset();
+
+		yield return null;
+	}
+
+	IEnumerator PutbackAfter(GameObject g, float t) {
+		yield return new WaitForSeconds(t);
+		ObjectPool.instance.PoolObject(g);
+	}
+
+	void UpdateSwarmParameters() {
+		_swarm.speed = speed;
+		_swarm.sight = sight;
+		_swarm.flockDistance = flockDistance;
+		_swarm.collisionSize = collisionSize;
+		_swarm.huntDistance = huntDistance;
+		_swarm.agility = Mathf.Clamp01(agility);
+
+		Camera c = Camera.main;
+		Vector2 bl = c.ViewportToWorldPoint(new Vector3(0f,0f, c.transform.position.y)).Vector2XZ();
+		Vector2 tr = c.ViewportToWorldPoint(new Vector3(1f,1f, c.transform.position.y)).Vector2XZ();
+		_swarm.cameraArea = new Rect(bl, tr-bl);
+
+		_manager.RegisterKilled(_swarm.killed);
+	}
+
+	void Update () {
+		UpdateSwarmParameters();
+
+		Vector2 targetPos2D = target.position.Vector2XZ();
+//		_swarm.MoveBoids(targetPos2D, Time.deltaTime);
+
+		for (var i = 0; i < numberOfDots; i++) {
+			Vector3 pos = _hostiles[i].position;
+			pos.x = _swarm.Boids[i].pos.x;
+			pos.z = _swarm.Boids[i].pos.y;
+			_hostiles[i].position = pos;
+			_hostiles[i].rotation = Quaternion.LookRotation(_swarm.Boids[i].faceDirection.Vector3XZ(), Vector3.up);
+
+			float targetDist = Vector2.Distance(targetPos2D, _swarm.Boids[i].pos);
+			if (targetDist < _swarm.collisionSize) _manager.GameOver();
+
+			if (_swarm.Boids[i].exploded) {
+
+				GameObject g = ObjectPool.instance.GetObjectForType("Explosion", true);
+
+				if (g != null) {
+					g.transform.position = pos;
+					g.transform.rotation = transform.rotation;
+
+					StartCoroutine(PutbackAfter(g, 5f));
+				}
+			}
+		}
+	}
+}
---
+++ Boids/Swarm.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) 2016 robosoup
+// www.robosoup.com
+
+using UnityEngine;
+using System.Collections.Generic;
+using Unitilities;
+
+namespace Unitilities.Boids
+{
+    public class Swarm
+    {
+		public float sight = 200f;
+		public float speed = 12f;
+		public float flockDistance = 3f;
+		public float huntDistance = 10f;
+		public float collisionSize = 1f;
+		public float agility = 0.7f;
+		public Rect cameraArea;
+		public Rect swarmArea;
+		public int killed = 0;
+
+        public List<Boid> Boids = new List<Boid>();
+		public static List<Boid> AllBoids = new List<Boid>();
+
+        public Swarm(float hunterPercentage, Rect area, int number, int type)
+        {
+			swarmArea = area;
+
+			for (var i = 0; i < number; i++) {
+				Boid b = new Boid(this, i<(number * (int)(100f * hunterPercentage)/100), area, type);
+				Boids.Add(b);
+				AllBoids.Add(b);
+			}
+        }
+
+//        public void MoveBoids(Vector2 target, float deltaTime)
+//        {
+//			foreach (var boid in Boids) {
+//				boid.Respawn();
+//				boid.Move(Boids,target,deltaTime);
+//			}
+//        }
+
+		public static void MoveAllBoids(Vector2 target, float deltaTime, float cameraSize)
+		{
+			foreach (var boid in AllBoids) {
+				boid.Respawn();
+				boid.Move(AllBoids,target,deltaTime,cameraSize);
+			}
+		}
+
+    }
+}
