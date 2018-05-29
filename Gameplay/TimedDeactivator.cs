f4f50e520fc42141c047d6eacc1802d3af98a79d
Martin Pichlmair <pi@brokenrul.es>
2018-05-29 15:49:06 +0200

    - added timed deactivator

---
+++ Gameplay/TimedDeactivator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Unitilities.Gameplay {
+
+	public class TimedDeactivator : MonoBehaviour {
+
+		public float Delay = 1f;
+
+        private IEnumerator Deactivate(float delay) {
+            yield return new WaitForSeconds(delay);
+            gameObject.SetActive(false);
+        }
+
+		void Start() {
+			StartCoroutine(Deactivate(Delay));
+		}
+	}
+}
