1609d3c9fdb10bc849b09b239c36737f88d3b948
Martin Pichlmair <pi@brokenrul.es>
2019-06-21 09:36:04 +0200

    - added some convenience math functions

--- Math/Utils.cs
+++ Math/Utils.cs
@@ -32,34 +32,34 @@ namespace Unitilities.Utils {
 		public static float HALF_PI = Mathf.PI / 2f;
 
 		public static double Clamp(double x, double bottom, double top) {
-			return x < bottom ? bottom : ( x > top ? top : x );
+			return x < bottom ? bottom : (x > top ? top : x);
 		}
 		public static double Max(double x, double y) { return (x > y) ? x : y; }
 		public static double Min(double x, double y) { return (x < y) ? x : y; }
 
-		public static Vector3 TrajectoryAtTime (Vector3 start, Vector3 startVelocity, float time) {
-		    return start + startVelocity*time + Physics.gravity*time*time*0.5f;
+		public static Vector3 TrajectoryAtTime(Vector3 start, Vector3 startVelocity, float time) {
+			return start + startVelocity * time + Physics.gravity * time * time * 0.5f;
 		}
 
 		public static float AngleBetween(Vector2 fromVector2, Vector2 toVector2) {
 			float ang0 = Mathf.Atan2(fromVector2.y, fromVector2.x);
 			float ang1 = Mathf.Atan2(toVector2.y, toVector2.x);
-			return Mathf.Atan2(Mathf.Sin(ang0-ang1), Mathf.Cos(ang0-ang1));
+			return Mathf.Atan2(Mathf.Sin(ang0 - ang1), Mathf.Cos(ang0 - ang1));
 		}
-		
+
 		public static float AngleBetween(float fromAngle, float toAngle) {
 			return UnwrapRadian(toAngle - fromAngle);
 		}
-		
+
 		public static float UnwrapRadian(float r) {
-			r = r % Mathf.PI*2.0f;
+			r = r % Mathf.PI * 2.0f;
 			if (r > Mathf.PI)
-				r -= Mathf.PI*2.0f;
+				r -= Mathf.PI * 2.0f;
 			else if (r < -Mathf.PI)
-				r += Mathf.PI*2.0f;
+				r += Mathf.PI * 2.0f;
 			return r;
 		}
-		
+
 		public static float DegToRad(float degrees) {
 			return degrees * (Mathf.PI / 180.0f);
 		}
@@ -69,34 +69,32 @@ namespace Unitilities.Utils {
 		}
 
 		public static float IncrementToward(float n, float target, float a) {
-			if (Mathf.Abs(n-target) < Mathf.Epsilon)
+			if (Mathf.Abs(n - target) < Mathf.Epsilon)
 				return n;
 			else {
-				float dir = Mathf.Sign (target - n);
+				float dir = Mathf.Sign(target - n);
 				n += a * dir;
-				return (dir == Mathf.Sign(target-n)) ? n : target;
+				return (dir == Mathf.Sign(target - n)) ? n : target;
 			}
 		}
 
 		public static Vector2 IncrementToward(Vector2 n, Vector2 target, float a) {
-			float dist = (target-n).magnitude;
+			float dist = (target - n).magnitude;
 			if (dist < Mathf.Epsilon) return target;
-			return n + Vector2.ClampMagnitude(target-n, Mathf.Min(dist, a));
+			return n + Vector2.ClampMagnitude(target - n, Mathf.Min(dist, a));
 		}
 
 		public static Vector3 IncrementToward(Vector3 n, Vector3 target, float a) {
-			float dist = (target-n).magnitude;
+			float dist = (target - n).magnitude;
 			if (dist < Mathf.Epsilon) return target;
-			return n + Vector3.ClampMagnitude(target-n, Mathf.Min(dist, a));
+			return n + Vector3.ClampMagnitude(target - n, Mathf.Min(dist, a));
 		}
 
-		public static float Mix(float x, float y, float a)
-		{
-			return (x + a * (y-x));
+		public static float Mix(float x, float y, float a) {
+			return (x + a * (y - x));
 		}
 
-		public static Vector3 BezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3)
-		{
+		public static Vector3 BezierPoint(float t, Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3) {
 			float u = 1.0f - t;
 			float tt = t * t;
 			float uu = u * u;
@@ -108,8 +106,7 @@ namespace Unitilities.Utils {
 			p += ttt * p3; //fourth term
 			return p;
 		}
-		public static Vector2 BezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
-		{
+		public static Vector2 BezierPoint(float t, Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3) {
 			float u = 1.0f - t;
 			float tt = t * t;
 			float uu = u * u;
@@ -127,22 +124,22 @@ namespace Unitilities.Utils {
 		public static float NormalizedLinearADSR(float a, float df, float s, float r, float factor) {
 			float f = Mathf.Clamp01(factor);
 			/* a: duration 0 -> 1 .. df: 1 -> df .. s: duration df -> df .. r: duration 0.5 -> 0 */
-			float de = (1f-r-s);
-			float d = (1f-r-s-a);
-			float se = (1f-r);
+			float de = (1f - r - s);
+			float d = (1f - r - s - a);
+			float se = (1f - r);
 
-			return f <  a ? f/a : 
-				 ( f < de ? ( df + (d+a-f)/d * (1f-df) ) : 
-				 ( f < se ? df : 
-				 ( (1f-f)/r * df )));
+			return f < a ? f / a :
+				 (f < de ? (df + (d + a - f) / d * (1f - df)) :
+				 (f < se ? df :
+				 ((1f - f) / r * df)));
 		}
 		public static float NormalizedLinearASR(float a, float s, float r, float factor) {
-			return NormalizedLinearADSR(a,1f,s,r,factor);
+			return NormalizedLinearADSR(a, 1f, s, r, factor);
 		}
 
 		public static Vector2i DirectionToVector(int i) {
-			return new Vector2i( (i > 0 && i < 4) ? 1 : (i > 4) ? -1 : 0, 
-								 (i > 2 && i < 6) ? 1 : (i == 2 || i == 6) ? 0 : -1 );
+			return new Vector2i((i > 0 && i < 4) ? 1 : (i > 4) ? -1 : 0,
+								 (i > 2 && i < 6) ? 1 : (i == 2 || i == 6) ? 0 : -1);
 		}
 
 		/* 
@@ -153,19 +150,76 @@ namespace Unitilities.Utils {
  			t=(x^(x>>7)); x=y; y=z; z=w; w=v;
  			v=(v^(v<<6))^(t^(t<<13)); return (y+y+1)*v;}
 		*/
-	}
-}
 
-public static class Boundsxtension
-{
-	public static bool ContainBounds(this Bounds bounds, Bounds target)
-	{
-		return bounds.Contains(target.min) && bounds.Contains(target.max);
-	}
-	public static bool ContainBoundsXY(this Bounds bounds, Bounds target)
-	{
-		return target.min.x >= bounds.min.x && target.max.x < bounds.max.x
-			&& target.min.y >= bounds.min.y && target.max.y < bounds.max.y;
 
 	}
+	public static class Extensions {
+		public static Vector3Int Div(this Vector3Int lhs, int rhs) {
+			return new Vector3Int(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
+		}
+
+		public static bool Overlaps(this RectInt rect, RectInt other) {
+			return !(rect.max.x < other.min.x || rect.min.x > other.max.x ||
+					  rect.max.y < other.min.y || rect.min.y > other.max.y);
+		}
+		public static RectInt IncludingPoint(this RectInt rect, Vector2Int point) {
+			/* exception for zero size rectints (i.e. first inclusion) */
+			if (rect.width == 0 || rect.height == 0) { rect.width = rect.height = 1; rect.position = point; return rect; }
+
+			rect.xMax = Mathi.Max(rect.xMax, point.x + 1);
+			rect.yMax = Mathi.Max(rect.yMax, point.y + 1);
+			rect.xMin = Mathi.Min(rect.xMin, point.x);
+			rect.yMin = Mathi.Min(rect.yMin, point.y);
+			return rect;
+		}
+		public static Vector2Int XY(this Vector3Int vector) { return new Vector2Int(vector.x, vector.y); }
+		public static Vector2Int XZ(this Vector3Int vector) { return new Vector2Int(vector.x, vector.z); }
+		public static Vector3Int XY(this Vector2Int vector) { return new Vector3Int(vector.x, vector.y, 0); }
+
+		public static Vector3Int[] Neighbours8(this Vector3Int vector) {
+			// clockwise starting from top left
+
+			Vector3Int[] neighbours = new Vector3Int[8];
+			neighbours[0] = vector + Vector3Int.up;
+			neighbours[1] = vector + Vector3Int.right + Vector3Int.up;
+			neighbours[2] = vector + Vector3Int.right;
+			neighbours[3] = vector + Vector3Int.right + Vector3Int.down;
+			neighbours[4] = vector + Vector3Int.down;
+			neighbours[5] = vector + Vector3Int.left + Vector3Int.down;
+			neighbours[6] = vector + Vector3Int.left;
+			neighbours[7] = vector + Vector3Int.left + Vector3Int.up;
+
+			return neighbours;
+		}
+
+		public static Vector3[] Neighbours8(this Vector3 vector) {
+			// clockwise starting from top left
+
+			Vector3[] neighbours = new Vector3[8];
+			neighbours[0] = vector + Vector3.left + Vector3.up;
+			neighbours[1] = vector + Vector3.up;
+			neighbours[2] = vector + Vector3.right + Vector3.up;
+			neighbours[3] = vector + Vector3.right;
+			neighbours[4] = vector + Vector3.right + Vector3.down;
+			neighbours[5] = vector + Vector3.down;
+			neighbours[6] = vector + Vector3.left + Vector3.down;
+			neighbours[7] = vector + Vector3.left;
+
+			return neighbours;
+		}
+	}
+
+	public static class Boundsxtension {
+		public static bool ContainBounds(this Bounds bounds, Bounds target) {
+			return bounds.Contains(target.min) && bounds.Contains(target.max);
+		}
+		public static bool ContainBoundsXY(this Bounds bounds, Bounds target) {
+			return target.min.x >= bounds.min.x && target.max.x < bounds.max.x
+				&& target.min.y >= bounds.min.y && target.max.y < bounds.max.y;
+
+		}
+	}
+
 }
+
+
