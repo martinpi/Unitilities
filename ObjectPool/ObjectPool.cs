11eff9301fc28f431ed67aeaa8c6feda7cd2d8e5
Martin Pichlmair <pi@brokenrul.es>
2021-02-25 13:59:44 +0100

    - added string comparer

--- ObjectPool/ObjectPool.cs
+++ ObjectPool/ObjectPool.cs
@@ -28,72 +28,68 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-public class ObjectPool : MonoBehaviour
-{
-	
+public class ObjectPool : MonoBehaviour {
+
 	public static ObjectPool instance;
-	
+
 	/// <summary>
 	/// The object prefabs which the pool can handle.
 	/// </summary>
 	public GameObject[] objectPrefabs;
-	
+
 	/// <summary>
 	/// The pooled objects currently available.
 	/// </summary>
 	public List<GameObject>[] pooledObjects;
-	
+
 	/// <summary>
 	/// The amount of objects of each type to buffer.
 	/// </summary>
 	public int[] amountToBuffer;
-	
+
 	public int defaultBufferAmount = 3;
-	
+
 	/// <summary>
 	/// The container object that we will keep unused pooled objects so we dont clog up the editor with objects.
 	/// </summary>
 	protected GameObject containerObject;
-	
-	void Awake ()
-	{
+
+	void Awake() {
 		instance = this;
 	}
-	
+
 	private bool _initialized = false;
-	
-	void Start () {
+
+	void Start() {
 		if (!_initialized) Init();
 	}
-	
+
 	public void Init() {
 		if (_initialized) return;
 		_initialized = true;
 		containerObject = gameObject; //new GameObject("ObjectPool");
-		
+
 		//Loop through the object prefabs and make a new list for each one.
 		//We do this because the pool can only support prefabs set to it in the editor,
 		//so we can assume the lists of pooled objects are in the same order as object prefabs in the array
 		pooledObjects = new List<GameObject>[objectPrefabs.Length];
-		
+
 		int i = 0;
-		foreach ( GameObject objectPrefab in objectPrefabs )
-		{
-			pooledObjects[i] = new List<GameObject>(); 
-			
+		foreach (GameObject objectPrefab in objectPrefabs) {
+			pooledObjects[i] = new List<GameObject>();
+
 			int bufferAmount;
-			
-			if(i < amountToBuffer.Length) bufferAmount = amountToBuffer[i];
+
+			if (i < amountToBuffer.Length) bufferAmount = amountToBuffer[i];
 			else
 				bufferAmount = defaultBufferAmount;
-			
-			for ( int n=0; n<bufferAmount; n++)
-			{
+
+			for (int n = 0; n < bufferAmount; n++) {
 				GameObject newObj = Instantiate(objectPrefab) as GameObject;
 				newObj.name = objectPrefab.name;
 				PoolObject(newObj);
 			}
-			
+
 			i++;
 		}
 	}
@@ -111,15 +107,11 @@ public class ObjectPool : MonoBehaviour
 	/// <param name='onlyPooled'>
 	/// If true, it will only return an object if there is one currently pooled.
 	/// </param>
-	public GameObject GetObjectForType ( string objectType , bool onlyPooled )
-	{
-		for(int i=0; i<objectPrefabs.Length; i++)
-		{
+	public GameObject GetObjectForType(string objectType, bool onlyPooled) {
+		for (int i = 0; i < objectPrefabs.Length; i++) {
 			GameObject prefab = objectPrefabs[i];
-			if(prefab.name == objectType)
-			{				
-				if(pooledObjects[i].Count > 0)
-				{
+			if (prefab.name == objectType) {
+				if (pooledObjects[i].Count > 0) {
 					GameObject pooledObject = pooledObjects[i][0];
 					pooledObjects[i].RemoveAt(0);
 					// pooledObject.transform.parent = null;
@@ -128,32 +120,31 @@ public class ObjectPool : MonoBehaviour
 					pooledObject.name = objectType; // set the name to be the same as the prefab so it can be re-pooled later
 
 					return pooledObject;
-					
-				} else if(!onlyPooled) {
+
+				} else if (!onlyPooled) {
 					return Instantiate(objectPrefabs[i]) as GameObject;
 				}
-				
+
 				break;
-				
+
 			}
 		}
-		
+
 		//If we have gotten here either there was no object of the specified type or non were left in the pool with onlyPooled set to true
+		Debug.LogError($"Can't retrieve object. Object pool for prefab named '{objectType}' doesn't exist.");
 		return null;
 	}
-	
+
 	/// <summary>
 	/// Pools the object specified.  Will not be pooled if there is no prefab of that name.
 	/// </summary>
 	/// <param name='obj'>
 	/// Object to be pooled.
 	/// </param>
-	public bool PoolObject ( GameObject obj )
-	{
-		for ( int i=0; i<objectPrefabs.Length; i++)
-		{
+	public bool PoolObject(GameObject obj) {
+		for (int i = 0; i < objectPrefabs.Length; i++) {
 			if (objectPrefabs[i].name != obj.name) continue;
-			
+
 			obj.SetActive(false);
 			obj.transform.SetParent(containerObject.transform);
 
@@ -161,6 +152,7 @@ public class ObjectPool : MonoBehaviour
 			return true;
 		}
 
+		Debug.LogError($"Can't pool object. Object pool for prefab named '{obj.name}' doesn't exist.");
 		return false;
 	}
 }
